1c27e7ab1d4f9e1771e5b54546791d51bfac3c3b
EggyBread <ruzzy2@hotmail.com>
2015-06-29 22:08:09 +1000

    Added colour detection for red and blue with preview

--- amaze.py
+++ amaze.py
@@ -13,12 +13,20 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    # convert to hsv and find range of colors
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    red_threshold = cv2.inRange(hsv,np.array((0, 80, 80)), np.array((40, 255, 255)))
+    blue_threshold = cv2.inRange(hsv,np.array((100, 80, 80)), np.array((140, 255, 255)))
+
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #grayscale
     ret,threshold = cv2.threshold(gray,95,255,cv2.THRESH_BINARY) #threshold
     erosion = cv2.erode(threshold,kernel,iterations = 1)
 
     # Display the resulting frame
+    cv2.imshow('red',red_threshold)
+    cv2.imshow('blue',blue_threshold)
+
     cv2.imshow('grayscale',gray)
     cv2.imshow('threshold',threshold)
     cv2.imshow('erosion',erosion)
