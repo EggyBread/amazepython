6de21561599fd5ae9899b48182e0c78aa8aa8236
EggyBread <ruzzy2@hotmail.com>
2015-07-02 23:49:21 +1000

    Finished the work except for sending path information down the line

--- amaze.py
+++ amaze.py
@@ -5,37 +5,41 @@ import cv2
 from Queue import Queue
 from PIL import Image
 
+# Define some constants
+ENDPOINT_RADIUS = 16
+
 def iswhite(value):
-	if value == (255,255,255) or value == (255,255,255,255): # Remove the alpha channel later as it adds to calculation time
-		return True
+    print value
+    if value == (255,255,255): # Remove the alpha channel later as it adds to calculation time
+        return True
 
 def getadjacent(n):
-	x,y = n
-	return [(x-1,y),(x,y-1),(x+1,y),(x,y+1)]
+    x,y = n
+    return [(x-1,y),(x,y-1),(x+1,y),(x,y+1)]
 
 def BFS(start, end, pixels):
-	queue = Queue()
-	queue.put([start]) # Wrapping the start tuple in a list
+    queue = Queue()
+    queue.put([start]) # Wrapping the start tuple in a list
 
-	while not queue.empty():
-		path = queue.get()
-		pixel = path[-1]
+    while not queue.empty():
+        path = queue.get()
+        pixel = path[-1]
 
-		if pixel == end:
-			return path
+        if pixel == end:
+            return path
 
-		for adjacent in getadjacent(pixel):
-			x,y = adjacent
-			try:
-				if iswhite(pixels[x,y]):
-					pixels[x,y] = (127,127,127) # see note
-					new_path = list(path)
-					new_path.append(adjacent)
-					queue.put(new_path)
-			except IndexError:
-				pass
+        for adjacent in getadjacent(pixel):
+            x,y = adjacent
+            try:
+                if iswhite(pixels[x,y]):
+                    pixels[x,y] = (127,127,127) # see note
+                    new_path = list(path)
+                    new_path.append(adjacent)
+                    queue.put(new_path)
+            except IndexError:
+                pass
 
-	print "Queue has been exhausted. No answer was found."
+    print "Queue has been exhausted. No answer was found."
 
 cap = cv2.VideoCapture(0)
 cap.set(3,1024) #set horizontal resolution
@@ -51,7 +55,7 @@ while(True):
     # Convert to hsv and find range of colors
     hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     green_threshold = cv2.inRange(hsv,np.array((45, 63, 63)), np.array((75, 255, 255)))
-    blue_threshold = cv2.inRange(hsv,np.array((90, 63, 63)), np.array((120, 255, 255)))
+    blue_threshold = cv2.inRange(hsv,np.array((90, 95, 95)), np.array((120, 255, 255)))
     
     green_threshold_copy = green_threshold.copy()
     blue_threshold_copy = blue_threshold.copy()
@@ -61,6 +65,9 @@ while(True):
     blue_contours, blue_hierarchy = cv2.findContours(blue_threshold,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
     # Finding contour with maximum area and store it as best_cnt
+    best_green_cnt = None
+    best_blue_cnt = None
+
     green_max_area = 0
     for cnt in green_contours:
         area = cv2.contourArea(cnt)
@@ -76,50 +83,66 @@ while(True):
             best_blue_cnt = cnt
 
     # Finding centroids of best_cnt and draw a circle there
-    M = cv2.moments(best_green_cnt)
-    cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
-    cv2.circle(frame,(cx,cy),5,(255,0,0),-1)
-    start = (cx,cy)
-    print start
+    if best_green_cnt is not None:
+        M = cv2.moments(best_green_cnt)
+        cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+        start = (cx,cy)
+        print start
 
-    M = cv2.moments(best_blue_cnt)
-    cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
-    cv2.circle(frame,(cx,cy),5,(0,255,0),-1)
-    end = (cx,cy)
-    print end
+    if best_blue_cnt is not None:
+        M = cv2.moments(best_blue_cnt)
+        cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+        
+        end = (cx,cy)
+        print end
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #grayscale
     ret,threshold = cv2.threshold(gray,95,255,cv2.THRESH_BINARY) #threshold
     erosion = cv2.erode(threshold,kernel,iterations = 1)
 
-    cv2.circle(erosion,start,15,(255,255,255),-1)
-    cv2.circle(erosion,end,15,(255,255,255),-1)
+    try:
+        # Draw endpoint markers on raw
+        cv2.circle(frame,start,ENDPOINT_RADIUS,(255,0,0),2)
+        cv2.circle(frame,end,ENDPOINT_RADIUS,(0,255,0),2)
+
+        # Draw white over endpoints on processed frame
+        cv2.circle(erosion,start,ENDPOINT_RADIUS,(255,255,255),-1)
+        cv2.circle(erosion,end,ENDPOINT_RADIUS,(255,255,255),-1)
+    except NameError:
+        pass
 
     # Display the resulting frame
+    cv2.imshow('raw',frame)
+
     cv2.imshow('green',green_threshold_copy)
     cv2.imshow('blue',blue_threshold_copy)
 
-    cv2.imshow('grayscale',gray)
-    cv2.imshow('threshold',threshold)
-    cv2.imshow('erosion',erosion)
+    cv2.imshow('processed',erosion)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-base_img = Image.fromarray(erosion)
+raw = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+problem = Image.fromarray(erosion).convert('RGB')
+
+base_img = problem
 base_pixels = base_img.load()
 
 path = BFS(start, end, base_pixels)
 
-path_img = Image.fromarray(erosion)
-path_pixels = path_img.load()
+path_pro = problem
+path_pro_pixels = path_pro.load()
+
+path_raw = raw
+path_raw_pixels = path_raw.load()
 
 for position in path:
-	x,y = position
-	path_pixels[x,y] = (255,0,0) # green
+    x,y = position
+    path_pro_pixels[x,y] = (255,0,0) # Red
+    path_raw_pixels[x,y] = (255,0,0)
 
-path_img.save("solution.png")
-
+path_pro.save("solution_processed.png")
+path_raw.save("solution_raw.png")
 
 # When everything done, release the capture
 cv2.imwrite("testgray.png",gray)
